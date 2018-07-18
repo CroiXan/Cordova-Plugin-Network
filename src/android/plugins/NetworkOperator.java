d16bcac7df477ac18ca478b58db6f385e1048243
Mattias Contreras <mvcontreras@entel.cl>
2018-07-18 17:35:22 -0400

    Android - get Operator Code from (TelephonyManager)Network Operator

---
+++ src/android/plugins/NetworkOperator.java
@@ -0,0 +1,49 @@
+/**
+ */
+package plugins;
+
+import android.app.Activity;
+import org.apache.cordova.CallbackContext;
+import org.apache.cordova.CordovaPlugin;
+import org.json.JSONObject;
+import org.json.JSONArray;
+import org.json.JSONException;
+import android.content.Context;
+import android.telephony.TelephonyManager;
+
+
+public class NetworkOperator extends CordovaPlugin {
+
+  @Override
+  public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
+	  
+	if(action.equals("getNetworkOperator")) {
+		
+		Context context = this.cordova.getActivity().getApplicationContext();
+		TelephonyManager manager = (TelephonyManager) context.getSystemService(Context.TELEPHONY_SERVICE);
+		
+		String carrier = "NA";
+		String networkCode = "NA";
+		String countryCode = "NA";
+		
+		if (manager.getSimState() != 1) {
+			carrier = manager.getSimOperatorName();
+			networkCode = manager.getNetworkOperator().substring(3,5);
+			countryCode = manager.getNetworkOperator().substring(0,3);
+		}
+		
+		JSONObject result = new JSONObject();
+		
+		result.put("carrier", carrier);
+		result.put("networkCode", networkCode);
+		result.put("countryCode", countryCode);
+		
+		callbackContext.sendPluginResult(result);
+	  
+		return true;
+    } else {
+		return false;
+	}
+    
+  }
+}
