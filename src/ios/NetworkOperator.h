d99ba3c7160048967a93b3fe150bfddf3c850cdd
Mattias Contreras <mvcontreras@entel.cl>
2018-07-19 15:26:19 -0400

    IOS function

---
+++ src/ios/NetworkOperator.h
@@ -0,0 +1,8 @@
+#import <Cordova/CDVPlugin.h>
+#import <Cordova/CDV.h>
+
+@interface NetworkOperator : CDVPlugin {
+}
+- (void)getNetworkOperator:(CDVInvokedUrlCommand *)command;
+
+@end
---
+++ src/ios/NetworkOperator.m
@@ -0,0 +1,31 @@
+#import "NetworkOperator.h"
+
+#import <Cordova/CDVAvailability.h>
+#import <CoreTelephony/CTCarrier.h>
+#import <CoreTelephony/CTTelephonyNetworkInfo.h>
+
+@implementation NetworkOperator
+
+- (void)getNetworkOperator:(CDVInvokedUrlCommand *)command {
+
+    NSString* words = [command.arguments objectAtIndex:0];
+    NSLog(@"*******js says: %@", words);
+	
+	CTTelephonyNetworkInfo *phoneInfo = [[CTTelephonyNetworkInfo alloc] init];
+	CTCarrier *phoneCarrier = [phoneInfo subscriberCellularProvider];
+
+	NSString * carrierName = [phoneCarrier carrierName];
+    NSString * mobileNetworkCode = [phoneCarrier mobileNetworkCode];
+    NSString * mobileCountryCode = [phoneCarrier mobileCountryCode];
+
+	NSDictionary *operatorInfo = [NSDictionary dictionaryWithObjectsAndKeys:
+		carrierName,@"carrier",
+		mobileNetworkCode,@"networkCode",
+		mobileNetworkCode,@"countryCode",
+	nil];
+	
+    CDVPluginResult *result = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsDictionary:operatorInfo];
+    [self.commandDelegate sendPluginResult:result callbackId:command.callbackId];
+}
+
+@end
